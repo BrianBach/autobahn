6d920b93586c49a594b0dbcb36f45e36a0d67cf1
grnet.kalligeros <grnet.kalligeros@70dc47f8-d969-49c3-b45f-d65fb9ce666a>
2010-01-07 09:47:15 +0000

    git-svn-id: svn+ssh://svn.geant.net/GEANT/SA2/AutoBAHN/AutoBAHN-base/trunk@2741 70dc47f8-d969-49c3-b45f-d65fb9ce666a

--- Framework/src/main/resources/start.sh
+++ Framework/src/main/resources/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dorg.apache.cxf.Logger=org.apache.cxf.common.logging.Log4jLogger -classpath .;lib\* net.geant2.jra3.framework.Framework start
+java -Dorg.apache.cxf.Logger=org.apache.cxf.common.logging.Log4jLogger -classpath .:lib/* net.geant2.jra3.framework.Framework start
