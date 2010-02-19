8923aae3aad1eeac6f3b738f5ce23f2f592cbbe2
grnet.kalligeros <grnet.kalligeros@70dc47f8-d969-49c3-b45f-d65fb9ce666a>
2010-02-19 13:13:50 +0000

    Fixed PasswordCallback classes to retrieve password from properties file
    git-svn-id: svn+ssh://svn.geant.net/GEANT/SA2/AutoBAHN/AutoBAHN-base/trunk@2811 70dc47f8-d969-49c3-b45f-d65fb9ce666a

--- Common/src/main/java/net/geant/autobahn/edugain/ClientPasswordCallback.java
+++ Common/src/main/java/net/geant/autobahn/edugain/ClientPasswordCallback.java
@@ -1,12 +1,18 @@
 package net.geant.autobahn.edugain;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
+import org.apache.log4j.Logger;
 import org.apache.ws.security.WSPasswordCallback;
+import java.util.Properties;
+
+import net.geant.autobahn.edugain.Edugain;
 
 /**
  * Holds a password for secured communication.
@@ -16,14 +22,27 @@ import org.apache.ws.security.WSPasswordCallback;
  */
 public class ClientPasswordCallback implements CallbackHandler {
 
-	final String PASSWORD = "edugain";
-
+	private final static Logger log = Logger.getLogger(ClientPasswordCallback.class);
+	static public String CLIENT_PROPERTIES = "etc/edugain/client-sec.properties";
+	static public String PASSWORD_PROPERTY = "org.apache.ws.security.crypto.merlin.keystore.password";
+	static public String PASSWORD = null;
+	
 	
 	/* (non-Javadoc)
 	 * @see javax.security.auth.callback.CallbackHandler#handle(javax.security.auth.callback.Callback[])
 	 */
 	@SuppressWarnings("deprecation")
 	public void handle(Callback[] arg0) throws IOException,	UnsupportedCallbackException {
+		
+		Properties properties = new Properties();
+		
+		try {
+			properties.load(new FileInputStream(CLIENT_PROPERTIES));
+			PASSWORD = properties.getProperty(PASSWORD_PROPERTY);
+						
+		} catch (FileNotFoundException e) {
+			log.error("Client side security properties not found: " + e.getMessage());
+		}
 		
 		WSPasswordCallback pc = (WSPasswordCallback)arg0[0];
 		pc.setPassword(PASSWORD);
--- Common/src/main/java/net/geant/autobahn/edugain/ServerPasswordCallback.java
+++ Common/src/main/java/net/geant/autobahn/edugain/ServerPasswordCallback.java
@@ -1,11 +1,15 @@
 package net.geant.autobahn.edugain;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
+import org.apache.log4j.Logger;
 import org.apache.ws.security.WSPasswordCallback;
 
 /**
@@ -16,7 +20,10 @@ import org.apache.ws.security.WSPasswordCallback;
  */
 public class ServerPasswordCallback implements CallbackHandler {
 
-	final String PASSWORD = "edugain";
+	private final static Logger log = Logger.getLogger(ClientPasswordCallback.class);
+	static public String SERVER_PROPERTIES = "etc/edugain/client-sec.properties";
+	static public String PASSWORD_PROPERTY = "org.apache.ws.security.crypto.merlin.keystore.password";
+	static public String PASSWORD = null;
 	
 	
 	/* (non-Javadoc)
@@ -24,6 +31,16 @@ public class ServerPasswordCallback implements CallbackHandler {
 	 */
 	@SuppressWarnings("deprecation")
 	public void handle(Callback[] arg0) throws IOException, UnsupportedCallbackException {
+		
+		Properties properties = new Properties();
+		
+		try {
+			properties.load(new FileInputStream(SERVER_PROPERTIES));
+			PASSWORD = properties.getProperty(PASSWORD_PROPERTY);
+						
+		} catch (FileNotFoundException e) {
+			log.error("Server side security properties not found: " + e.getMessage());
+		}
 		
 		WSPasswordCallback pc = (WSPasswordCallback)arg0[0];
 		pc.setPassword(PASSWORD);
