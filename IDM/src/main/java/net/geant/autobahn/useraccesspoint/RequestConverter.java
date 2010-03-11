05e336d68241a58f7a037ad14afa48e96e043c38
grnet.stamos <grnet.stamos@70dc47f8-d969-49c3-b45f-d65fb9ce666a>
2010-03-11 10:10:02 +0000

    Added verifications for sane values of various reservation parameters (ABAHN-86)
    git-svn-id: svn+ssh://svn.geant.net/GEANT/SA2/AutoBAHN/AutoBAHN-base/trunk@2830 70dc47f8-d969-49c3-b45f-d65fb9ce666a

--- IDM/src/main/java/net/geant/autobahn/useraccesspoint/RequestConverter.java
+++ IDM/src/main/java/net/geant/autobahn/useraccesspoint/RequestConverter.java
@@ -33,19 +33,34 @@ public class RequestConverter {
 		
         PortDAO pdao = daos.getPortDAO();
         Port start = pdao.getByBodID(req.getStartPort());
+        if (start == null) {
+            throw new UserAccessPointException("reservation start port " + req.getStartPort() + " could not be found");
+        }
         Port end = pdao.getByBodID(req.getEndPort());
+        if (end == null) {
+            throw new UserAccessPointException("reservation end port " + req.getEndPort() + " could not be found");
+        }
         
         HomeDomainReservation resv = new HomeDomainReservation(start, end,
                 req.getStartTime(), req.getEndTime(), req.getPriority().ordinal());
         
         resv.setDescription(req.getDescription());
         resv.setCapacity(req.getCapacity());
+        if (req.getCapacity() <= 0) {
+            throw new UserAccessPointException("reservation capacity cannot be negative or zero");
+        }
         resv.setMaxDelay(req.getMaxDelay());
+        if (req.getMaxDelay() <= 0) {
+            throw new UserAccessPointException("reservation max delay cannot be negative or zero");
+        }
         resv.setResiliency(req.getResiliency().name());
         resv.setBidirectional(req.isBidirectional());
         resv.setUserInclude(req.getUserInclude());
         resv.setUserExclude(req.getUserExclude());
         resv.setUserVlanId(req.getUserVlanId());
+        if (req.getUserVlanId() < 0) {
+            throw new UserAccessPointException("reservation user VLAN cannot be negative");
+        }
         
         return resv;
 	}
