529cb55a79cfe085eade37203261e07ee550ed07
psnc.czapka <psnc.czapka@70dc47f8-d969-49c3-b45f-d65fb9ce666a>
2010-07-09 11:49:35 +0000

    ABAHN-114
    git-svn-id: svn+ssh://svn.geant.net/GEANT/SA2/AutoBAHN/AutoBAHN-base/trunk@2961 70dc47f8-d969-49c3-b45f-d65fb9ce666a

---
+++ WebGUI/src/main/webapp/scripts/jscalendar/js/lang/pl.js
@@ -0,0 +1,64 @@
+// autor: Piotr kwiatkowski
+// www: http://pasjonata.net
+
+Calendar.LANG("pl", "Polish", {
+
+        fdow: 1,                // pierwszy dzieñ tygodnia; 0 = Niedziela, 1 = Poniedzia³ek, itd.
+
+        goToday: "Idzie Dzisiaj",
+
+        today: "Dzisiaj",         
+
+        wk: "wk",
+
+        weekend: "0,6",         // 0 = Niedziela, 1 = Poniedzia³ek, itd.
+
+        AM: "am",
+
+        PM: "pm",
+
+        mn : [ "Styczeñ",
+               "Luty",
+               "Marzec",
+               "Kwiecieñ",
+               "Maj",
+               "Czerwiec",
+               "Lipiec",
+               "Sierpieñ",
+               "Wrzesieñ",
+               "PaŸdziernik",
+               "Listopad",
+               "Grudzieñ" ],
+
+        smn : [ "Sty",
+                "Lut",
+                "Mar",
+                "Kwi",
+                "Maj",
+                "Cze",
+                "Lip",
+                "Sie",
+                "Wrz",
+                "PaŸ",
+                "Lis",
+                "Gru" ],
+
+        dn : [ "Niedziela",
+               "Poniedzia³ek",
+               "Wtorek",
+               "Œroda",
+               "Czwartek",
+               "Pi¹tek",
+               "Sobota",
+               "Niedziela" ],
+
+        sdn : [ "Ni",
+                "Po",
+                "Wt",
+                "Œr",
+                "Cz",
+                "Pi",
+                "So",
+                "Ni" ]
+
+});
