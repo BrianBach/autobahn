16c84293c905902aae0321608aef405a3b8bfad5
psnc.balcerkiewicz <psnc.balcerkiewicz@70dc47f8-d969-49c3-b45f-d65fb9ce666a>
2010-07-16 09:11:39 +0000

    Removed as it is already moved to diffrent location
    git-svn-id: svn+ssh://svn.geant.net/GEANT/SA2/AutoBAHN/AutoBAHN-base/trunk@2975 70dc47f8-d969-49c3-b45f-d65fb9ce666a

--- TP-GMPLS/src/main/java/net/geant/autobahn/constraints/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "constraints.autobahn.geant.net")
-package net.geant.autobahn.constraints;
--- TP-GMPLS/src/main/java/net/geant/autobahn/tool/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://tool.autobahn.geant.net/")
-package net.geant.autobahn.tool;
