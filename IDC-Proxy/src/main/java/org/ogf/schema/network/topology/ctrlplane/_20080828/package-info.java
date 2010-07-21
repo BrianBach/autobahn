03e3080c2f449426daf0440b01cb5cf790714796
grnet.stamos <grnet.stamos@70dc47f8-d969-49c3-b45f-d65fb9ce666a>
2010-07-21 10:53:45 +0000

    Removed Proxy interface, integrated IDCP functionality to IDM (ABAHN-137)
    git-svn-id: svn+ssh://svn.geant.net/GEANT/SA2/AutoBAHN/AutoBAHN-base/trunk@3000 70dc47f8-d969-49c3-b45f-d65fb9ce666a

--- IDC-Proxy/src/main/java/org/ogf/schema/network/topology/ctrlplane/_20080828/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ogf.org/schema/network/topology/ctrlPlane/20080828/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.ogf.schema.network.topology.ctrlplane._20080828;
