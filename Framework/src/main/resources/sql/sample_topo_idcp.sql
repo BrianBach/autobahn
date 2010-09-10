b71cacfd594265abd45f3b7712f56188c1f9e259
psnc.balcerkiewicz <psnc.balcerkiewicz@70dc47f8-d969-49c3-b45f-d65fb9ce666a>
2010-09-10 07:07:07 +0000

    + topology used for idcp testing
    git-svn-id: svn+ssh://svn.geant.net/GEANT/SA2/AutoBAHN/AutoBAHN-base/trunk@3102 70dc47f8-d969-49c3-b45f-d65fb9ce666a

---
+++ Framework/src/main/resources/sql/sample_topo_idcp.sql
@@ -0,0 +1,38 @@
+
+-- simple topology consisting of two client nodes and two idcp client nodes
+
+INSERT INTO node VALUES (1, NULL, NULL, 'test-node', NULL, NULL, NULL, NULL, NULL, NULL, 'test-node');
+INSERT INTO node VALUES (2, NULL, NULL, 'client-node1', NULL, NULL, NULL, NULL, NULL, NULL, 'client-node1');
+INSERT INTO node VALUES (3, NULL, NULL, 'client-node2', NULL, NULL, NULL, NULL, NULL, NULL, 'client-node2');
+INSERT INTO node VALUES (4, NULL, NULL, 'idcp-node', NULL, NULL, NULL, NULL, NULL, NULL, 'idcp-node');
+
+INSERT INTO generic_interface VALUES (1, NULL, NULL, NULL, 1, 'p1', NULL, 1000000000, NULL, NULL, 
+'http://localhost:8080/autobahn/interdomain', false);
+INSERT INTO generic_interface VALUES (2, NULL, NULL, NULL, 2, 'host-port1', NULL, 1000000000, NULL, NULL, 
+'http://client-domain1.com', true);
+INSERT INTO generic_interface VALUES (3, NULL, NULL, NULL, 3, 'host-port2', NULL, 1000000000, NULL, NULL, 
+'http://client-domain2.com', true);
+INSERT INTO generic_interface VALUES (4, NULL, NULL, NULL, 4, 'idcp-port1', NULL, 1000000000, NULL, NULL,
+'http://client-domain.controlplane.net/oscars', true);
+INSERT INTO generic_interface VALUES (5, NULL, NULL, NULL, 4, 'idcp-port2', NULL, 1000000000, NULL, NULL,
+'http://client-domain.controlplane.net/oscars', true);
+
+INSERT INTO generic_link VALUES (1, NULL, 1, 2, NULL, false, 0);
+INSERT INTO generic_link VALUES (2, NULL, 1, 3, NULL, false, 0);
+INSERT INTO generic_link VALUES (3, NULL, 1, 4, NULL, false, 0);
+INSERT INTO generic_link VALUES (4, NULL, 1, 5, NULL, false, 0);
+
+INSERT INTO eth_link VALUES (1, NULL, false, false, 0);
+INSERT INTO eth_link VALUES (2, NULL, false, false, 0);
+INSERT INTO eth_link VALUES (3, NULL, false, false, 0);
+INSERT INTO eth_link VALUES (4, NULL, false, false, 0);
+
+INSERT INTO vlan VALUES (1, NULL, NULL, 100, 200);
+INSERT INTO vlan VALUES (2, NULL, NULL, 100, 200);
+INSERT INTO vlan VALUES (3, NULL, NULL, 100, 200);
+INSERT INTO vlan VALUES (4, NULL, NULL, 100, 200);
+
+INSERT INTO spanning_tree VALUES (1, 1, NULL, 0);
+INSERT INTO spanning_tree VALUES (2, 2, NULL, 0);
+INSERT INTO spanning_tree VALUES (3, 3, NULL, 0);
+INSERT INTO spanning_tree VALUES (4, 4, NULL, 0);
