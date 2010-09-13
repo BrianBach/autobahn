6761b283c54469cad232dcfe0df438c940b7e564
grnet.stamos <grnet.stamos@70dc47f8-d969-49c3-b45f-d65fb9ce666a>
2010-09-13 15:42:32 +0000

    git-svn-id: svn+ssh://svn.geant.net/GEANT/SA2/AutoBAHN/AutoBAHN-base/trunk@3115 70dc47f8-d969-49c3-b45f-d65fb9ce666a

--- Framework/src/main/java/net/geant/autobahn/framework/commands/IdcpTopologyCommand.java
+++ Framework/src/main/java/net/geant/autobahn/framework/commands/IdcpTopologyCommand.java
@@ -27,7 +27,7 @@ public class IdcpTopologyCommand implements AutobahnCommand {
 	@Override
 	public String execute(Framework autobahn, String[] args) {
 	
-		OscarsClient oscars = new OscarsClient();
+		OscarsClient oscars = new OscarsClient(args[0]);
 		try {
 			List<Link> links = oscars.getTopology();
 			
