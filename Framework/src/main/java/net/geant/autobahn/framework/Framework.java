d3dae19548a64e295eb4506700a42311ce21655e
grnet.stamos <grnet.stamos@70dc47f8-d969-49c3-b45f-d65fb9ce666a>
2011-03-23 14:31:02 +0000

    Used telnet negotiation commands to instruct telnet client to not display password (ABAHN-300)
    git-svn-id: svn+ssh://svn.geant.net/GEANT/SA2/AutoBAHN/AutoBAHN-base/trunk@3362 70dc47f8-d969-49c3-b45f-d65fb9ce666a

--- Framework/src/main/java/net/geant/autobahn/framework/Framework.java
+++ Framework/src/main/java/net/geant/autobahn/framework/Framework.java
@@ -1,10 +1,13 @@
 package net.geant.autobahn.framework;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
@@ -136,6 +139,9 @@ public class Framework {
 
 		PrintWriter out = null;
 		BufferedReader in = null;
+		
+		//Used for telnet codes
+        BufferedWriter rawout = null;
 
 		while (true) {
 
@@ -151,8 +157,11 @@ public class Framework {
 				}
 
 				out = new PrintWriter(client.getOutputStream(), true);
-				in = new BufferedReader(new InputStreamReader(client
-						.getInputStream()));
+				in = new BufferedReader(new InputStreamReader(
+                        client.getInputStream(), "ISO-8859-1"));
+
+                rawout = new BufferedWriter(new OutputStreamWriter(
+                        client.getOutputStream(), "ISO-8859-1"));
 
 				out.println("-------------------------");
 				out.println(" Autobahn framework '08");
@@ -165,20 +174,42 @@ public class Framework {
 				 */
 				out.print("Give password:");
 				out.flush();
-				String line = in.readLine();
-				if(!line.equals(properties.getProperty("framework.password"))) {
+				
+				// Send IAC WILL ECHO
+                rawout.write((char)0xff);
+                rawout.write((char)0xfb);
+                rawout.write((char)0x01);
+                rawout.flush();
+                out.flush();
+                
+                // Telnet client will probably respond with IAC DONT ECHO
+                // (0xff 0xfd 0x01)
+                // so we have to remove these commands from incoming stream
+                String line = trimTelnetNegotiationCommands(in.readLine());
+                if(!line.equals(properties.getProperty("framework.password"))) {
                     client.close();
                     log.warn("wrong password");
                     client = null;
                     continue;
-				}
+                }
+
+                // Send IAC WONT ECHO
+                rawout.write((char)0xff);
+                rawout.write((char)0xfc);
+                rawout.write((char)0x01);
+                rawout.flush();
+                
+                out.println();
+                out.flush();
 			}
 
 			try {
 				out.print(PROMPT);
 				out.flush();
 
-				String line = in.readLine();
+                // Telnet client will probably initially send 
+				// IAC DO ECHO so read and ignore it
+				String line = trimTelnetNegotiationCommands(in.readLine());
 
 				String[] elems = line.split("\\s");
 				String cmd = elems[0].trim().toLowerCase();
@@ -197,9 +228,18 @@ public class Framework {
 			}
 
 			if (stop) {
-				out.close();
-				in.close();
-				client.close();
+				if (out != null) {
+				    out.close();
+				}
+				if (in != null) {
+				    in.close();
+				}
+                if (rawout != null) {
+                    rawout.close();
+                }
+                if (client != null) {
+                    client.close();
+                }
 				client = null;
 
 				if (shutdown) {
@@ -297,4 +337,43 @@ public class Framework {
 		
 		System.exit(0);
 	}
+
+    /**
+     * Removes Telnet negotiation 3-byte sequences from supplied String
+     * Searches for IAC byte and removes it along with next two bytes
+     * 
+     * @param line
+     * @return String without telnet negotiation bytes
+     */
+    public String trimTelnetNegotiationCommands(String line) {
+        if (line == null) {
+            return null;
+        }
+        
+        byte[] byteline;
+        try {
+            byteline = line.getBytes("ISO-8859-1");
+        } catch (UnsupportedEncodingException e) {
+            return line;
+        }
+        for (int i=0; i<byteline.length; i++) {
+            // 0xff is IAC (Interpret as command) character
+            // It is followed by one or two bytes (RFC 854)
+            // We assume 3-byte sequences will be received:
+            // IAC <type of operation> <option>
+            // 0xff is stored as -1 in signed arithmetic
+            if (byteline[i] == -1) {
+                try {
+                    if (i>0) {
+                        return trimTelnetNegotiationCommands(line.substring(0,i)+line.substring(i+3));
+                    } else {
+                        return trimTelnetNegotiationCommands(line.substring(i+3));
+                    }
+                } catch (IndexOutOfBoundsException e) {
+                    return "";
+                }
+            }
+        }
+        return line;
+    }
 }
