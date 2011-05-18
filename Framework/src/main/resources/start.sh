857940cbb68176db7eafc8b92ade3accf06c100b
grnet.stamos <grnet.stamos@70dc47f8-d969-49c3-b45f-d65fb9ce666a>
2011-05-18 13:31:51 +0000

    Fixed problem with commandline option (ABAHN-324)
    git-svn-id: svn+ssh://svn.geant.net/GEANT/SA2/AutoBAHN/AutoBAHN-base/trunk@3418 70dc47f8-d969-49c3-b45f-d65fb9ce666a

--- Framework/src/main/resources/start.sh
+++ Framework/src/main/resources/start.sh
@@ -1,6 +1,37 @@
-#!/bin/sh
+#!/bin/bash
+OLD_PATH=`pwd`
+
+#this loads the path where the script file is located
+FILE_PATH=`dirname $0`
+
+#this is the framework properties file
+FILE=etc/framework.properties
+CONFIG=etc/cxf/cxf.xml
+
+cd $FILE_PATH
 CP=.
 for i in `ls lib/*.jar`; do CP=$CP:$i; done
-rm -f ./nohup.out;touch nohup.out
-nohup java -Dcxf.config.file=etc/cxf/cxf.xml -Dorg.apache.cxf.Logger=org.apache.cxf.common.logging.Log4jLogger -classpath $CP net.geant.autobahn.framework.Framework start &
-tail -f nohup.out
+rm -f ./nohup.out;
+
+#the command to start the autobahn
+AUTOBAHN="java -Dcxf.config.file=$CONFIG -Dorg.apache.cxf.Logger=org.apache.cxf.common.logging.Log4jLogger -classpath $CP net.geant.autobahn.framework.Framework"
+
+#we read the property file eliminating the comments
+#then we find the line containing the framework.commandLine and we only keep its value
+line=`sed 's/[#!].*$//g' $FILE |grep "framework.commandLine"`
+framework_commandLine=`echo $line |sed 's/framework.commandLine//g'| tr -d '' | tr -d '=' | tr -d ':'`
+
+#if the framework_commandLine property has been set to interactive
+#we do not start the autobahn as daemon
+if [ "$framework_commandLine" == "interactive" ]
+then
+    echo "Starting autobahn"    
+    $AUTOBAHN
+    cd $OLD_PATH
+else
+    echo "Starting autobahn daemon"
+    touch ./nohup.out
+    nohup $AUTOBAHN &
+    cd $OLD_PATH
+    tail -f $FILE_PATH/nohup.out
+fi
