eb4987bfc3a161bf174c92ada8e417db36208a21
grnet.stamos <grnet.stamos@70dc47f8-d969-49c3-b45f-d65fb9ce666a>
2011-06-13 19:06:33 +0000

    git-svn-id: svn+ssh://svn.geant.net/GEANT/SA2/AutoBAHN/AutoBAHN-base/trunk@3450 70dc47f8-d969-49c3-b45f-d65fb9ce666a

--- Clients/src/main/java/net/geant/autobahn/administration/AdministrationSampleClient.java
+++ Clients/src/main/java/net/geant/autobahn/administration/AdministrationSampleClient.java
@@ -4,6 +4,10 @@ import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 import javax.xml.ws.soap.SOAPBinding;
 
+import net.geant.autobahn.network.Link;
+
+import java.util.List;
+
 public class AdministrationSampleClient {
 
     private static final QName SERVICE_NAME = new QName("http://administration.autobahn.geant.net/", "AdministrationService");
@@ -18,17 +22,83 @@ public class AdministrationSampleClient {
         adm = service.getPort(AdministrationPort, Administration.class);
     }
 
-    private void sampleReservation() {
-
-        System.out.println("ServiceID:\n" + adm.getServices());
-    }
-    
     public static void main(String args[]) throws Exception {
 
-        AdministrationSampleClient instance = new AdministrationSampleClient("http://localhost:1234/autobahn/administration");
+        System.out.println("Give IDM to connect to:");
+        byte byteStr[] = new byte[50];
+        System.in.read(byteStr);
+        String idm = (new String(byteStr).trim());
 
-        instance.sampleReservation();
-        //System.out.println(instance.getPorts());
+        System.out.println("Give IDM port:");
+        byteStr = new byte[50];
+        System.in.read(byteStr);
+        String port = (new String(byteStr).trim());
+        
+        if (idm == null || idm.equals("")) {
+            idm = "109.105.111.62";
+        }
+        if (port == null || port.equals("")) {
+            port = "8080";
+        }
+        
+        AdministrationSampleClient instance = new AdministrationSampleClient("http://"+idm+":"+port+"/autobahn/administration");
+        System.out.println("Connecting to "+idm+":"+port);
+        
+        System.out.println("\n---getLog():");
+        String ad_log = instance.adm.getLog(true);
+        System.out.println(ad_log);
+        
+        System.out.println("\n---getServices():");
+        List<ServiceType> serv = instance.adm.getServices();
+        if (serv != null) {
+            for (int i=0; i<serv.size(); i++) {
+                if (serv.get(i) != null) {
+                    System.out.println(i+": "+serv.get(i).getBodID());
+                }
+            }
+        }
+        
+        System.out.println("\n---getProperties():");
+        List<KeyValue> props = instance.adm.getProperties();
+        if (props != null) {
+            for (int i=0; i<props.size(); i++) {
+                if (props.get(i) != null) {
+                    System.out.println(i+": "+props.get(i).getKey()+"="+props.get(i).getValue());
+                }
+            }
+        }
+        
+        System.out.println("\n---getStatistics():");
+        StatisticsType stype = instance.adm.getStatistics(true);
+        if (stype != null) {
+            System.out.println("getAverageInter:"+stype.getAverageInter()+
+                    "\ngetAverageIntra:"+stype.getAverageIntra());
+            if (stype.getInter() != null) {
+                System.out.println("Inter length:"+stype.getInter().size());
+            }
+            if (stype.getIntra() != null) {
+                System.out.println("Intra length:"+stype.getIntra().size());
+            }
+        }
+        
+        System.out.println("\n---getTopology():");
+        List<Link> topo = instance.adm.getTopology();
+        if (topo != null) {
+            for (int i=0; i<topo.size(); i++) {
+                System.out.println(i+": "+topo.get(i));
+            }
+        }
+        
+        System.out.println("\n---getStatus():");
+        Status stat = instance.adm.getStatus();
+        if (stat != null) {
+            System.out.println("getDomain:"+stat.getDomain()+
+                    "\ngetLatitude:"+stat.getLatitude()+
+                    "\ngetLongitude:"+stat.getLongitude());
+            if (stat.getNeighbors() != null) {
+                System.out.println("getNeighbors length:"+stat.getNeighbors().size());
+            }
+        }
         
         System.exit(0);
     }
